5b5c7e297f540d1d8e5219d91d6bc162c0ef52e9
Bayode Aderinola <bayodesegun@gmail.com>
2018-02-09 16:05:06 +0100

    convert indentation to tabs

--- portfolio_api/tests/test_models.py
+++ portfolio_api/tests/test_models.py
@@ -23,60 +23,60 @@ class UserModelTest(TestCase):
 		self.user.email='nerd@example.com'
 		self.user.last_name = 'User'
 		with self.assertRaises(ValidationError):
-            self.user.save()
+			self.user.save()
 
-    def test_last_name_is_required(self):
+	def test_last_name_is_required(self):
 		# The modified User model must require last name
 		self.user.email='nerd@example.com'
 		self.user.first_name = 'Nerd'
 		with self.assertRaises(ValidationError):
-            self.user.save()
+			self.user.save()
 
-    def test_email_is_required(self):
+	def test_email_is_required(self):
 		# The modified User model must require first name
 		self.user.first_name="Nerd"
 		self.user.last_name = 'User'
 		with self.assertRaises(ValidationError):
-            self.user.save()
+			self.user.save()
 
-    def user_short_bio_field_exists_with_correct_max_lenght(self):
-    	field = self.user._meta.get_field('short_bio')
-    	self.assertTrue(isinstance(field, TextField))
-    	max_length = self.user._meta.get_field('short_bio').max_length
-    	self.assertEquals(max_length, 300)
+	def user_short_bio_field_exists_with_correct_max_lenght(self):
+		field = self.user._meta.get_field('short_bio')
+		self.assertTrue(isinstance(field, TextField))
+		max_length = self.user._meta.get_field('short_bio').max_length
+		self.assertEquals(max_length, 300)
 
-    def user_image_field_exists_and_saves_image_correctly(self):
-    	field = self.user._meta.get_field('image')
-    	self.assertTrue(isinstance(field, ImageField))
-    	with open('test.jpg', 'r') as f:
-    		img = File(f)
-    		self.valid_user.image = img
-    		self.valid_user.save()
-    		# file will be saved in a subdirectory of media named after the user id
-    		# current timestamp will also be added to filename to ensure uniqueness
-    		self.assertIn(os.path.join(BASE_DIR, "/media/uploads/{0!s}/test".format(self.valid_user.id)), self.valid_user.image.path)
+	def user_image_field_exists_and_saves_image_correctly(self):
+		field = self.user._meta.get_field('image')
+		self.assertTrue(isinstance(field, ImageField))
+		with open('test.jpg', 'r') as f:
+			img = File(f)
+			self.valid_user.image = img
+			self.valid_user.save()
+			# file will be saved in a subdirectory of media named after the user id
+			# current timestamp will also be added to filename to ensure uniqueness
+			self.assertIn(os.path.join(BASE_DIR, "/media/uploads/{0!s}/test".format(self.valid_user.id)), self.valid_user.image.path)
 
-    def user_resume_field_exists(self):
-    	field = self.user._meta.get_field('resume')
-    	self.assertTrue(isinstance(field, FileField))
-    	with open('test.doc', 'w') as f:
-    		resume = File(f)
-    		resume.write('Test Resume')
-    		self.valid_user.resume = resume
-    		self.valid_user.save()
-    		# file will be saved in a subdirectory of media named after the user id
-    		# current timestamp will also be added to filename to ensure uniqueness
-    		self.assertIn(os.path.join(BASE_DIR, "/media/uploads/{0!s}/resume".format(self.valid_user.id)), self.valid_user.resume.path)
-    	resume.delete()
+	def user_resume_field_exists(self):
+		field = self.user._meta.get_field('resume')
+		self.assertTrue(isinstance(field, FileField))
+		with open('test.doc', 'w') as f:
+			resume = File(f)
+			resume.write('Test Resume')
+			self.valid_user.resume = resume
+			self.valid_user.save()
+			# file will be saved in a subdirectory of media named after the user id
+			# current timestamp will also be added to filename to ensure uniqueness
+			self.assertIn(os.path.join(BASE_DIR, "/media/uploads/{0!s}/resume".format(self.valid_user.id)), self.valid_user.resume.path)
+		resume.delete()
 
-    def user_website_field_exists(self):
-    	field = self.user._meta.get_field('website')
-    	self.assertTrue(isinstance(field, URLField))
+	def user_website_field_exists(self):
+		field = self.user._meta.get_field('website')
+		self.assertTrue(isinstance(field, URLField))
 
-    def user_github_profile_field_exists(self):
-    	field = self.user._meta.get_field('github_profile')
-    	self.assertTrue(isinstance(field, URLField))
+	def user_github_profile_field_exists(self):
+		field = self.user._meta.get_field('github_profile')
+		self.assertTrue(isinstance(field, URLField))
 
-    def user_linkedin_profile_field_exists(self):
-    	field = self.user._meta.get_field('linkedin_profile')
-    	self.assertTrue(isinstance(field, URLField))
+	def user_linkedin_profile_field_exists(self):
+		field = self.user._meta.get_field('linkedin_profile')
+		self.assertTrue(isinstance(field, URLField))
