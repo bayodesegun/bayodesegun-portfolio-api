3869942efadf516ca9280094a9d90b54ee5bbccc
Bayode Aderinola <bayodesegun@gmail.com>
2018-02-12 12:31:30 +0100

    Add tests and definitions for Project & Screensot models

--- portfolio_api/models.py
+++ portfolio_api/models.py
@@ -4,7 +4,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 def file_upload_path(instance, filename):
-    return 'uploads/user_{0}/{1}'.format(instance.id, filename.split('/')[-1])
+    id = None
+    if isinstance(instance, User):
+        id = instance.id
+    elif isinstance(instance, Screenshot):
+        id = instance.project.user.id
+    
+    return 'uploads/user_{0}/{1}'.format(id, filename.split('/')[-1])
 
 class User(AbstractUser):
     email = models.EmailField(blank=False)
@@ -16,3 +22,19 @@ class User(AbstractUser):
     website = models.URLField(blank=True)
     github_profile = models.URLField(blank=True)
     linkedin_profile = models.URLField(blank=True)
+
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    short_description = models.CharField(max_length=100)
+    full_description = models.TextField(max_length=1000, blank=True)
+    tech_stack = models.CharField(max_length=200)
+    role = models.TextField(max_length=300)
+    private = models.BooleanField(default=False)
+    live_url = models.URLField(blank=True)
+    repo_url = models.URLField(blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Screenshot(models.Model):
+    image = models.ImageField(upload_to=file_upload_path, blank=False)
+    caption = models.CharField(max_length=50)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
--- portfolio_api/tests/test_models.py
+++ portfolio_api/tests/test_models.py
@@ -1,11 +1,12 @@
 import os
 from django.test import TestCase
 from django.core.exceptions import ValidationError
-from django.db.models import ImageField, FileField, URLField, TextField
+from django.db.models import ImageField, FileField, URLField, TextField, CharField, BooleanField, ForeignKey
 from django.core.files import File
 
 # Create your tests here.
-from portfolio_api.models import User
+from portfolio_api.models import User, Project, Screenshot
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class UserModelTest(TestCase):
 	""" This class defines the test suite for the custom User model """
@@ -14,7 +15,6 @@ class UserModelTest(TestCase):
 		# set up data and variables available for all test methods
 		# a user with all required fields set
 		self.valid_user = User.objects.create(username='geek', password='password', first_name='Geek', last_name='User', email='geek@example.com')
-		self.current_dir = os.path.dirname(os.path.abspath(__file__))
 
 	def setUp(self):
 		# runs before all test methods, with clean data
@@ -47,13 +47,12 @@ class UserModelTest(TestCase):
 	def test_short_bio_field_exists_with_correct_max_lenght(self):
 		field = self.user._meta.get_field('short_bio')
 		self.assertTrue(isinstance(field, TextField))
-		max_length = self.user._meta.get_field('short_bio').max_length
-		self.assertEquals(max_length, 300)
+		self.assertEquals(field.max_length, 300)
 
 	def test_image_field_exists_and_saves_image_correctly(self):
 		field = self.user._meta.get_field('image')
 		self.assertTrue(isinstance(field, ImageField))
-		img = File(open(os.path.join(self.current_dir, 'test.jpg'), 'rb'))
+		img = File(open(os.path.join(CURRENT_DIR, 'test.jpg'), 'rb'))
 		self.valid_user.image = img
 		self.valid_user.save()
 		
@@ -64,10 +63,10 @@ class UserModelTest(TestCase):
 		img.close
 		self.valid_user.image.delete(save=True)
 
-	def test_resume_field_exists(self):
+	def test_resume_field_exists_and_saves_file_correctly(self):
 		field = self.user._meta.get_field('resume')
 		self.assertTrue(isinstance(field, FileField))
-		resume = File(open(os.path.join(self.current_dir, 'test.doc'), 'rb'))
+		resume = File(open(os.path.join(CURRENT_DIR, 'test.doc'), 'rb'))
 		self.valid_user.resume = resume
 		self.valid_user.save()
 		
@@ -89,3 +88,92 @@ class UserModelTest(TestCase):
 	def test_linkedin_profile_field_exists(self):
 		field = self.user._meta.get_field('linkedin_profile')
 		self.assertTrue(isinstance(field, URLField))
+
+class ScreenshotModelTest(TestCase):
+	""" This class defines the test suite for the Screenshot model """
+	@classmethod
+	def setUpTestData(self):
+		# set up data and variables available for all test methods
+		self.user = User.objects.create(username='genius', password='password', first_name='Genius', last_name='User', email='genius@example.com')
+		self.project = Project.objects.create(user=self.user, name='XYZ Portfolios', short_description='XYZ portfolio site', role='FE: designed the front end')
+		self.shot = Screenshot.objects.create(project=self.project, caption='User login page')
+		
+	def test_image_field_is_present_and_saves_correctly(self):
+		field = self.user._meta.get_field('image')
+		self.assertTrue(isinstance(field, ImageField))
+		img = File(open(os.path.join(CURRENT_DIR, 'test.jpg'), 'rb'))
+		self.shot.image = img
+		self.shot.save()
+		
+		# file will be saved in a subdirectory of media named after the user id
+		self.assertIn("uploads/user_{0!s}/test.jpg".format(self.user.id), self.shot.image.path)
+		
+		# close file and delete the uploaded file
+		img.close
+		self.shot.image.delete(save=True)
+
+	def test_caption_field_is_present_as_char_field_with_right_limit(self):
+		field = self.shot._meta.get_field('caption')
+		self.assertTrue(isinstance(field, CharField))
+		self.assertEquals(field.max_length, 50)
+
+	def test_project_field_is_present_as_foreign_key_field(self): # Screenshot belongs to Project (Many to One)
+		field = self.shot._meta.get_field('project')
+		self.assertTrue(isinstance(field, ForeignKey))
+
+class ProjectModelTest(TestCase):
+	""" This class defines the test suite for the custom Project model """
+	@classmethod
+	def setUpTestData(self):
+		# set up data and variables available for all test methods
+		self.user = User.objects.create(username='nomad', password='password', first_name='Nomad', last_name='User', email='nomad@example.com')
+		self.project = Project.objects.create(user=self.user, name='Portfolio Site', short_description='My portfolio site', role='Contributor, designed the db schema')
+		
+	def test_name_field_is_present_as_char_field_with_right_limit(self):
+		field = self.project._meta.get_field('name')
+		self.assertTrue(isinstance(field, CharField))
+		self.assertEquals(field.max_length, 50)
+		self.assertEquals(field.blank, False)
+
+	def test_short_description_field_is_present_as_text_field_with_right_limit(self):
+		field = self.project._meta.get_field('short_description')
+		self.assertTrue(isinstance(field, CharField))
+		self.assertEquals(field.max_length, 100)
+		self.assertEquals(field.blank, False)
+
+	def test_full_description_field_is_present_as_text_field_with_right_limit(self):
+		field = self.project._meta.get_field('full_description')
+		self.assertTrue(isinstance(field, TextField))
+		self.assertEquals(field.max_length, 1000)
+		self.assertEquals(field.blank, True)
+
+	def test_description_field_is_present_as_text_field_with_right_limit(self):
+		field = self.project._meta.get_field('tech_stack')
+		self.assertTrue(isinstance(field, CharField))
+		self.assertEquals(field.max_length, 200)
+		self.assertEquals(field.blank, False)
+
+	def test_role_field_is_present_as_text_field_with_right_limit(self):
+		field = self.project._meta.get_field('role')
+		self.assertTrue(isinstance(field, TextField))
+		self.assertEquals(field.max_length, 300)
+		self.assertEquals(field.blank, False)
+
+	def test_private_field_is_present_as_boolean_field_with_right_deafault(self):
+		field = self.project._meta.get_field('private')
+		self.assertTrue(isinstance(field, BooleanField))
+		self.assertEquals(field.default, False)
+
+	def test_live_url_field_is_present_as_text_field_with_right_default(self):
+		field = self.project._meta.get_field('live_url')
+		self.assertTrue(isinstance(field, URLField))
+		self.assertEquals(field.blank, True)
+
+	def test_repo_url_field_is_present_as_text_field_with_right_default(self):
+		field = self.project._meta.get_field('repo_url')
+		self.assertTrue(isinstance(field, URLField))
+		self.assertEquals(field.blank, True)
+
+	def test_user_field_is_present_as_foreign_key_field(self): # Project belongs to User (Many to One)
+		field = self.project._meta.get_field('user')
+		self.assertTrue(isinstance(field, ForeignKey))
